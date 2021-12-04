58ffb0667d94f7b9972986d3ed270785f582bab5
aalshawa97 <abdullahalshawa@gmail.com>
2021-12-04 13:37:28 -0800

    Text view updates

--- app/src/main/java/com/example/bmwapp/MainActivity.java
+++ app/src/main/java/com/example/bmwapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import com.example.bmwapp.data.LocationPojo;
 import com.example.bmwapp.data.remote.ApiClient;
 import com.example.bmwapp.data.remote.ApiInterface;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 
 import org.json.JSONException;
 
@@ -22,14 +24,23 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int DEFAULT_UPGRADE_INTERVAL = 30;
+    public static final int FAST_UPDATE_INTERVAL = 5;
     TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
     Switch sw_locationUpdates, sw_gps;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    //Variable to remember if we are tracking location or not.
+    boolean updateOn = false;
+
     static  String TAG = MainActivity.class.getSimpleName();
     //http://localsearch.azurewebsites.net/api/Locations
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //Give each UI variable a value
         tv_lat = findViewById(R.id.tv_lat);
         tv_lon = findViewById(R.id.tv_lon);
         tv_altitude = findViewById(R.id.tv_altitude);
@@ -40,6 +51,14 @@ public class MainActivity extends AppCompatActivity {
         tv_address = findViewById(R.id.tv_address);
         sw_gps = findViewById(R.id.sw_gps);
         sw_locationUpdates = findViewById(R.id.sw_locationsupdates);
+
+        //Set all properties of LocationRequest
+        LocationRequest locationRequest = new LocationRequest();
+
+        //How ofter does the default location check occur?
+        locationRequest.setInterval(1000 * 30);
+        //How often does the location check occur when set to the most frequent update?
+        locationRequest.setFastestInterval(1000 * 5);
 
         try {
             MyTask.getJSONObjectFromURL("https://localsearch.azurewebsites.net/api/Locations");
