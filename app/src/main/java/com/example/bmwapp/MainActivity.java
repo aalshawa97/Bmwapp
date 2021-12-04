5740d43c33dacc71eca44da0056be9a6947163cc
aalshawa97 <abdullahalshawa@gmail.com>
2021-12-04 12:01:51 -0800

    Find view by ID

--- app/src/main/java/com/example/bmwapp/MainActivity.java
+++ app/src/main/java/com/example/bmwapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.bmwapp.data.LocationPojo;
 import com.example.bmwapp.data.remote.ApiClient;
@@ -20,12 +22,25 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
+    Switch sw_locationUpdates, sw_gps;
     static  String TAG = MainActivity.class.getSimpleName();
     //http://localsearch.azurewebsites.net/api/Locations
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+        sw_gps = findViewById(R.id.sw_gps);
+        sw_locationUpdates = findViewById(R.id.sw_locationsupdates);
+
         try {
             MyTask.getJSONObjectFromURL("https://localsearch.azurewebsites.net/api/Locations");
         } catch (IOException e) {
